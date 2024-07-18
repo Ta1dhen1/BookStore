a6b7aba8e3280ba3f16660f58ad178ce31e1f9a6
Ta1dhen1 <Ta1dhen1@mail.ru>
2024-07-18 07:29:00 +0300

    fix

--- src/main/java/com/example/BookStore/services/CartService.java
+++ src/main/java/com/example/BookStore/services/CartService.java
@@ -48,7 +48,6 @@ public class CartService {
             cart.setQuantity(cart.getQuantity() + 1);
             cart.setTotalPrice(cart.getQuantity() * cart.getBook().getPrice());
         }
-        System.out.println(cart.getQuantity());
         cartRepository.save(cart);
     }
 
